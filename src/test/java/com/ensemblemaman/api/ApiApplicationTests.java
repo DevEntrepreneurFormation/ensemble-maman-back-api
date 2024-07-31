fce34a8a39918b4a971013df12019a82e1b60230
Samuel-Jr Ndolou <samuel-jr@MacBook-Pro-de-Samuel-Jr.local>
2024-07-31 09:11:29 +0200

    initialization of the project

---
+++ src/test/java/com/ensemblemaman/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensemblemaman.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
