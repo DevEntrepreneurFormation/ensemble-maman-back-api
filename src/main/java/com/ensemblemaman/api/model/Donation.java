1b44e74d89a32db12911a205e1406038a131053b
Burdy <burdy.goureige.ep@whoz.com>
2024-08-08 10:50:57 +0200

    api CRUD realisé pour les dons

--- src/main/java/com/ensemblemaman/api/model/Donation.java
+++ src/main/java/com/ensemblemaman/api/model/Donation.java
@@ -5,6 +5,7 @@ import com.ensemblemaman.api.model.ConstantDonation.GenderEnum;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -14,26 +15,43 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "donation")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "type_donation")
+@DiscriminatorColumn(name = "type_donation", discriminatorType = DiscriminatorType.STRING)
 public class Donation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+
+    @Column(nullable=false)
     public String name;
 
-    @Column(name="image_url")
+    @Column(name="image_url", nullable = false)
     public String imageUrl;
 
+    @Column(nullable=false)
     public int quantity;
-    
+
     @Enumerated(EnumType.STRING)
     public GenderEnum gender;
+
+    @Transient
+    public String getTypeDonation() {
+        if (this instanceof Clothe) {
+            return "CLOTHE";
+        } else if (this instanceof Hygiene) {
+            return "HYGIENE";
+        }
+        else if (this instanceof Toy) {
+            return "TOY";
+        }
+        return "UNKNOWN";
+    }
 }
 
--- src/main/java/com/ensemblemaman/api/service/DonationService.java
+++ src/main/java/com/ensemblemaman/api/service/DonationService.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ensemblemaman.api.model.Clothe;
 import com.ensemblemaman.api.model.Donation;
+import com.ensemblemaman.api.model.Hygiene;
+import com.ensemblemaman.api.model.Toy;
 import com.ensemblemaman.api.repository.DonationRepository;
 
 @Service
@@ -17,16 +20,70 @@ public class DonationService {
        return donationRepository.findAll();
     }
 
-    public Donation updateDonation(Long id, Donation data){
-        donationRepository.deleteById(id);
-        Donation donations = donationRepository.save(data);
-        // changer la donation
-        return donations;
-     };
+    public Optional<Clothe> updateClothe(Long id, Clothe clothe){
+        return donationRepository.findById(id)
+        .filter(Clothe.class::isInstance)
+        .map(Clothe.class::cast)
+        .map(existingClothe -> {
+            existingClothe.setName(clothe.getName());
+            existingClothe.setImageUrl(clothe.getImageUrl());
+            existingClothe.setQuantity(clothe.getQuantity());
+            existingClothe.setGender(clothe.getGender());
+            existingClothe.setName(clothe.getName());
+            existingClothe.setSize(clothe.getSize());
+            existingClothe.setState(clothe.getState());
 
-    public Donation createDonation(Donation data){
-        Donation donations = donationRepository.save(data);
-        return donations;
+        return donationRepository.save(existingClothe);
+      });
+    }
+
+   public Optional<Hygiene> updateHygiene(Long id, Hygiene hygiene) {
+    return donationRepository.findById(id)
+    .filter(Hygiene.class::isInstance)
+    .map(Hygiene.class::cast)
+    .map(existingClothe -> {
+        existingClothe.setName(hygiene.getName());
+        existingClothe.setImageUrl(hygiene.getImageUrl());
+        existingClothe.setQuantity(hygiene.getQuantity());
+        existingClothe.setGender(hygiene.getGender());
+        existingClothe.setName(hygiene.getName());
+        existingClothe.setDateExpiration(hygiene.getDateExpiration());
+        existingClothe.setCategory(hygiene.getCategory());
+
+    return donationRepository.save(existingClothe);
+  });
+   }
+
+    public Optional<Toy> updateToy(Long id, Toy toy){
+        return donationRepository.findById(id)
+        .filter(Toy.class::isInstance)
+        .map(Toy.class::cast)
+        .map(existingToy -> {
+            existingToy.setName(toy.getName());
+            existingToy.setImageUrl(toy.getImageUrl());
+            existingToy.setQuantity(toy.getQuantity());
+            existingToy.setGender(toy.getGender());
+            existingToy.setName(toy.getName());
+            existingToy.setAge(toy.getAge());
+            existingToy.setState(toy.getState());
+
+        return donationRepository.save(existingToy);
+      });
+    }
+
+    public Hygiene createHygieneProduct(Hygiene data){
+        System.out.println(data);
+        return donationRepository.save(data);
+    };
+
+    public Toy createToy(Toy data){
+        System.out.println(data);
+        return donationRepository.save(data);
+    };
+
+    public Clothe createClothe(Clothe data){
+        System.out.println(data);
+        return donationRepository.save(data);
     };
 
     public Long deleDonation(Long id){
@@ -35,6 +92,7 @@ public class DonationService {
     };
 
     public Optional<Donation> getOneDonation(Long id){
-        return donationRepository.findById(id);        
+        return donationRepository.findById(id);
+                
     };
 }
